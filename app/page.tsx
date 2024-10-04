e20d981214fef60884f8f508dc0d0d23a00e49c3
ShaYri12 <sharjeelb60@gmail.com>
2024-10-05 03:43:21 +0500

    slider done

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,19 @@
-import DirectGameserverConnections from "@/components/DirectGameserverConnections";
 import FAQ from "@/components/FAQ";
 import Footer from "@/components/Footer";
 import Hero from "@/components/Hero";
 import Pricing from "@/components/Pricing";
 import WhyChooseUs from "@/components/WhyChooseUs";
+import GameServerSlider from "../components/GameServerSlider";
 
 export default function Home() {
   return (
     <div>
-      <Hero/>
-      <WhyChooseUs/>
-      <Pricing/>
-      <FAQ/>
-      <DirectGameserverConnections/>
-      <Footer/>
+      {/* <Hero /> */}
+      <WhyChooseUs />
+      <Pricing />
+      <FAQ />
+      <GameServerSlider />
+      <Footer />
     </div>
   );
 }
