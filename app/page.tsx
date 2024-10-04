58cce0ecb99e004eae22a1997720d9c48bc9234c
ShaYri12 <sharjeelb60@gmail.com>
2024-10-05 03:44:39 +0500

    hero

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ import GameServerSlider from "../components/GameServerSlider";
 export default function Home() {
   return (
     <div>
-      {/* <Hero /> */}
+      <Hero />
       <WhyChooseUs />
       <Pricing />
       <FAQ />
