83d6c0ca333e5cabc2376fa954666fe6a7d74765
vikram kumar <vikramkumar8t8@gmail.com>
2024-10-06 00:48:56 +0500

    update

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -37,24 +37,27 @@ const Hero = () => {
       {/* Overlay */}
       <div className="absolute top-0 left-0 w-full h-full bg-black opacity-50 z-10"></div>
 
-      <Header />
-
       {/* Main Content */}
-      <div className="max-w-[1240px] py-10 sm:py-[60px] md:py-[100px] lg:py-0 w-full mx-auto gap-10 lg:gap-5 flex lg:flex-row flex-col items-center justify-between relative z-20">
-        <div className="w-full">
-          <h1 className="text-[40px] md:text-[64px] md:leading-[78px] font-semibold uppercase">
-            Ultra-low <br /> latency vpn
-          </h1>
-          <h3 className="mt-6 md:mt-12 text-[#01E4FD] text-[20px] sm:text-[24px] leading-[24px] font-semibold uppercase">
-            built for competitive gaming
-          </h3>
-          <a href="/#pricing" className="w-fit h-fit">
-            <button className="mt-6 md:mt-12 text-white text-[16px] sm:text-[18px] leading-[18px] font-medium w-[214px] h-[45px] sm:h-[54px] rounded-[70px] bg-[#01E4FD] hover:bg-[#00C0E5] transition duration-300 capitalize">
-              try for free
-            </button>
-          </a>
+      <div className="max-w-[1240px]  w-full mx-auto gap-10 lg:gap-5 flex lg:flex-row flex-col items-center justify-between relative z-20">
+        <div className="flex flex-col w-full">
+          <div className=" pb-[100px]">
+            <Header />
+          </div>
+          <div className="w-full pb-[20px] lg:pb-[100px]">
+            <h1 className="text-[40px] md:text-[64px] md:leading-[78px] font-semibold uppercase">
+              Ultra-low <br /> latency vpn
+            </h1>
+            <h3 className="mt-6 md:mt-12 text-[#01E4FD] text-[20px] sm:text-[24px] leading-[24px] font-semibold uppercase">
+              built for competitive gaming
+            </h3>
+            <a href="/#pricing" className="w-fit h-fit">
+              <button className="mt-6 md:mt-12 text-white text-[16px] sm:text-[18px] leading-[18px] font-medium w-[214px] h-[45px] sm:h-[54px] rounded-[70px] bgcustomone  transition duration-300 capitalize">
+                try for free
+              </button>
+            </a>
+          </div>
         </div>
-        <div className="w-full h-full">
+        <div className="w-full h-full ">
           <GlobeDemo />
         </div>
       </div>
